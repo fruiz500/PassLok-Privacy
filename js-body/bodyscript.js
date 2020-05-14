e09898308a4748540e0f4db1be1641e7f1faf6d2
Francisco Ruiz <fruiz500@gmail.com>
2020-05-14 16:39:31 -0500

    v2.4.18 again
    Let's see if bodyscript takes now

--- js-body/bodyscript.js
+++ js-body/bodyscript.js
@@ -19,53 +19,78 @@
 //  Clear out "sorry, no JavaScript" warning and display the type of source
 	showGreeting();
 
-//set global variable indicating if there is a Chrome sync data area. Works for Chrome and Firefox apps and extension
+//set global variable indicating if there is a Chrome sync data area. Works for Chrome apps and extension
 var ChromeSyncOn = false;
-if(chrome){
+if(isChrome){
 	if(chrome.storage){
 		if(chrome.storage.sync){
-			ChromeSyncOn = true
+			ChromeSyncOn = true;
+			retrieveAllSync();
 		}
 	}
 }
 
 //clears the no JavaScript warning and displays an initial message depending on the type of source
 function showGreeting(){
-	var protocol = window.location.protocol;
-	var msgStart = "Welcome to PassLok Privacy\r\n",
-		msgEnd = "\r\nSelect your user name and enter your Key. Then click OK";
+	var protocol = window.location.protocol,
+		msgStart = "Welcome to SeeOnce\r\n",
+		msgEnd = "\r\nEnter your Password and click OK";
 	if(protocol == 'file:'){
-		pwdMsg.textContent = msgStart + 'running from a saved file' + msgEnd
+		keyMsg.textContent = msgStart + 'running from a local file' + msgEnd
 	}else if(protocol == 'https:'){
-		pwdMsg.textContent = msgStart + 'downloaded from a secure server' + msgEnd
-	}else if(protocol == 'chrome-extension:' || protocol == 'moz-extension:'){
-		pwdMsg.textContent = msgStart + 'running as a Chrome or Firefox addon' + msgEnd
+		keyMsg.textContent = msgStart + 'downloaded from a secure server' + msgEnd
+	}else if(protocol == 'chrome-extension:'){
+		keyMsg.textContent = msgStart + 'running as a Chrome app' + msgEnd
 	}else{
-		mainTab.style.backgroundColor = '#ffd0ff';
-		pwdMsg.textContent = msgStart + 'WARNING: running from an insecure source!' + msgEnd
+		mainScr.style.backgroundColor = '#ffd0ff';
+		keyMsg.textContent = msgStart + 'WARNING: running from an insecure source!' + msgEnd
 	}
+	if(!isMobile) pwd.focus()
+
+	//display special greeting the first time the program runs. The Chrome app does this differently
+	if(!ChromeSyncOn){if(!localStorage){introGreeting();}else if(!localStorage['locDir']){introGreeting();}}
+}
+
+//special instructions the first time it runs
+function introGreeting(){
+	firstTimeKey.style.display = 'block';
+	keyMsg.textContent = 'The strength will appear here. Enter the Password and click OK';
+	if(isiPhone || isAndroidPhone){
+		keyScr.style.width = '100%';			//more space needed for phones
+		keyScr.style.height = '100%';
+		keyScr.style.top = 0;
+		keyScr.style.left = 0;
+		firstTimeKey.style.fontSize = 'large'	//smaller font
+	}else if(isMobile){
+		firstTimeKey.style.fontSize = 'large'
+	}
+}
+function cancelIntroGreeting(){
+	keyScr.style.width = '';
+	keyScr.style.height = '';
+	keyScr.style.top = '';
+	keyScr.style.left = '';
+	firstTimeKey.style.display = 'none'
 }
 
 //resizes text boxes so they fit within the window
 function textheight(){
 	var	fullheight = document.documentElement.clientHeight,
-		offsetheight = 392,
+		offsetheight = 400,
 		toolbarheight = 50;
-	if(isiPhone) offsetheight = offsetheight - 65;
-	lockBox.style.height = (fullheight - 300) * (isMobile ? 1 : 0.8) + 'px';
+	if(isiPhone) offsetheight = offsetheight - 70;
+	coverBox.style.height = fullheight - offsetheight + 50 + 'px';
 	if(niceEditor){
 		mainBox.style.height = fullheight - offsetheight - toolbarheight + 'px'
 	}else{
 		if(isMobile){
 			if(isAndroid && !isFile){
-				mainBox.style.height = fullheight - offsetheight + 40 + 'px';
-			}else if(isiPhone){
-				mainBox.style.height = fullheight - offsetheight - 10 + 'px';
+				mainBox.style.height = fullheight - offsetheight + 80 + 'px';
 			}else{
-				mainBox.style.height = fullheight - offsetheight + 'px';
+				mainBox.style.height = fullheight - offsetheight + 40 + 'px';
 			}
 		}else{
-			mainBox.style.height = fullheight - offsetheight - 10 + 'px';
+			mainBox.style.height = fullheight - offsetheight + 'px';
 		}
 	}
 }
@@ -88,24 +113,25 @@ function loadFileAsURL(){
 			if(URLFromFileLoaded.slice(0,2) == '==' && URLFromFileLoaded.slice(-2) == '=='){
 				var fileLink = document.createElement("a");
 				fileLink.download = fileName;
-				fileLink.href = "data:," + safeHTML(URLFromFileLoaded);			//filter before adding to the DOM
+				fileLink.href = "data:," + decryptSanitizer(URLFromFileLoaded);			//filter before adding to the DOM
 				fileLink.textContent = fileName;
 				mainBox.appendChild(fileLink)
 			}else{
 				var spacer = document.createElement("br"),
 					textDiv = document.createElement("div");
-				textDiv.textContent = safeHTML(URLFromFileLoaded).replace(/  /g,' &nbsp;');
+				textDiv.textContent = decryptSanitizer(URLFromFileLoaded).replace(/  /g,' &nbsp;');
 				mainBox.appendChild(spacer);
 				mainBox.appendChild(textDiv)
 			}
 		}else{
 			var fileLink = document.createElement("a");
 			fileLink.download = fileName;
-			fileLink.href = safeHTML(URLFromFileLoaded).replace(/=+$/,'');
+			fileLink.href = decryptSanitizer(URLFromFileLoaded).replace(/=+$/,'');
 			fileLink.textContent = fileName;
 			mainBox.appendChild(fileLink)
 		}
-	};
+		if(fileName.match(/.(plk|txt)/)) Decrypt()				//in case it is an encrypted message, attempt to decrypt it
+	}
 	if(fileToLoad.type.slice(0,4) == "text"){
 		fileReader.readAsText(fileToLoad, "UTF-8");
 		mainMsg.textContent = 'This is the content of file: ' + fileToLoad.name;
@@ -113,32 +139,6 @@ function loadFileAsURL(){
 		fileReader.readAsDataURL(fileToLoad, "UTF-8");
 		mainMsg.textContent = 'The file has been loaded in encoded form. It is NOT ENCRYPTED';
 	}
-	setTimeout(function(){
-		updateButtons();
-		if(decryptBtn.textContent == 'Decrypt') mainMsg.textContent = 'This file may contain an encrypted item';
-		if(verifyBtn.textContent == 'Unseal') mainMsg.textContent = 'This file may contain a sealed item';
-		if(secretShareBtn.textContent == 'Join') mainMsg.textContent = 'This file may contain a part. Add more parts if necessary';
-	},300);
-}
-
-//to load a file into the directory dialog
-function loadLockFile(){
-	var fileToLoad = lockFile.files[0],
-		fileReader = new FileReader();
-	fileReader.onload = function(fileLoadedEvent){
-		var fileName = fileToLoad.name;
-			URLFromFileLoaded = fileLoadedEvent.target.result,
-			escapedName = escapeHTML(fileName);
-		lockBox.textContent = '';
-		var fileLink = document.createElement('a');
-		fileLink.download = escapedName;
-		fileLink.href = safeHTML(URLFromFileLoaded).replace(/=+$/,'');
-		fileLink.textContent = escapedName;
-		lockBox.appendChild(fileLink)
-	};
-
-	fileReader.readAsDataURL(fileToLoad, "UTF-8");
-	lockMsg.textContent = 'File ' + escapedName + ' has been loaded'
 }
 
 //to load an image into the main box
@@ -152,7 +152,7 @@ function loadImage(){
 			return
 		}
 		var image = document.createElement("img");
-		image.src = safeHTML(URLFromFileLoaded).replace(/=+$/,'');
+		image.src = decryptSanitizer(URLFromFileLoaded).replace(/=+$/,'');
 		mainBox.appendChild(image)
 	};
 
--- js-body/initbuttons.js
+++ js-body/initbuttons.js
@@ -1,57 +1,36 @@
 ﻿// initialize things
 window.onload = function() {
 
-	if(isMobile){
-		imgSpacer.style.display = 'none';
-		mainSpacer.style.display = 'none';
-		partSpacer1.style.display = 'none';
-		partSpacer2.style.display = 'none';
-		decoyInSpacer1.style.display = 'none';
-		decoyInSpacer2.style.display = 'none';
-		decoyOutSpacer.style.display = 'none';
-		var bottomButtons = mainbuttonsbot.children;
-		for(var i = 0; i < bottomButtons.length; i++) bottomButtons[i].classList.add("narrow")
-	}else{
-		previewImg.style.width = "80%"					//smaller image on PCs
+	if(isiPhone || isAndroidPhone){				//screen is narrow, so use smaller type and buttons
+		keyMsg.style.fontSize = 'medium';
+		mainMsg.style.fontSize = 'medium';
+		mainMsg.style.height = '40px';
+		logo.style.height = '70px';
+		narrowButtons()
 	}
-	if(!isMobile || isChrome){						//search box in Help tab. Works on Android Chrome, but won't detect right
-		helpTopMobile.style.display = 'none';
-		helpTop.style.display = 'block';
-		helpSpace.style.display = 'block';
-	}
-	if(isAndroid){										//resize shift buttons on Android
-		extra2mainBtn.style.padding = '11px';
-		main2extraBtn.style.padding = '11px';
-	}
-	if(isiOS) encodeJPGBtn.style.display = 'none';	//JPG hide does not work on iOS
-	if(isiPhone || isAndroidPhone){					//to make things fit on narrow screens
-		anonLabel.textContent = ' Anon.  ';
-		modeLabel.style.display = 'none';
-		otherLabel.style.display = 'none';
-		greenLabel.textContent = 'Grn';
-		customLabel.textContent = 'Cust';
-		backgroundLabel.textContent = 'Bg.';
-		sentencesLabel.textContent = 'Sent.';
-		lockScr.style.top = "5%";
-		lockScr.style.left = "5%";
-		lockScr.style.width = "90%";
-		lockScr.style.height = "90%";
-	}
-	if(isiOS && isFile){								//don't display things that don't work on iOS app
-		introVideoText.style.display = 'none';
-		sendSMSBtn.style.display = 'none';
-	}
-	
-	//load field icons
-	pwdIntroIcon.src = eyeImg;
-	pwdIcon.src = eyeImg;
-	newKeyIcon.src = eyeImg;
-	decoyInIcon.src = eyeImg;
-	decoyOutIcon.src = eyeImg;
-	imageIcon.src = eyeImg;
+	if(isiOS || isSafari || isIE) makeChatBtn.style.display = 'none';		//webRTC not supported in these
 
+  //load field icons
+	showKey.src = eyeImg;
+	showOldKey.src = eyeImg;
+	
   //event listeners for buttons etc.
 	window.addEventListener('resize',textheight);
+
+   	showKey.addEventListener('click', showSec);
+
+	showOldKey.addEventListener('click', showOldSec);
+
+	acceptKeyBtn.addEventListener('click', acceptKey);
+
+	acceptOldKeyBtn.addEventListener('click', acceptOldKey);
+
+	cancelOldKeyBtn.addEventListener('click', cancelOldKey);
+
+	mainBox.addEventListener('paste', pasteItem);
+	function pasteItem() {setTimeout(function(){unlockItem();}, 0);}
+
+	mainBox.addEventListener('keyup', changeButtons);
 
 	mainFile.addEventListener('change', loadFileAsURL);
 	mainFile.addEventListener('click', function(){this.value = '';});
@@ -59,268 +38,61 @@ window.onload = function() {
 	imgFile.addEventListener('change', loadImage);
 	imgFile.addEventListener('click', function(){this.value = '';});
 
-	imageFile.addEventListener('change', importImage);
-	imageFile.addEventListener('click', function(){this.value = '';});
+	selectBtn.addEventListener('click', selectMain);
 
-	imageFileEmail.addEventListener('change', importImage);
-	imageFileEmail.addEventListener('click', function(){this.value = '';});
+   	clearBtn.addEventListener('click', clearMain);
 
-	lockFile.addEventListener('change', loadLockFile);
-	lockFile.addEventListener('click', function(){this.value = '';});
+	fileMode.addEventListener('click', toggleFileOptions);
 
-    encodePNGBtn.addEventListener('click', encodePNG);
+	helpBtn.addEventListener('click', main2help);
 
-	encodeJPGBtn.addEventListener('click', encodeJPG);
+	help2mainBtnTop.addEventListener('click', main2help);
 
-    decodeImgBtn.addEventListener('click', decodeImage);
+	help2mainBtnBottom.addEventListener('click', main2help);
 
-	imageIcon.addEventListener('click',function(){showPwd('image')});
-
-	showLockBtn.addEventListener('click', showLock);
-
-	selectMainBtn.addEventListener('click', selectMain);
-
-   	clearMainBtn.addEventListener('click', clearMain);
-
-	showLockBtnBasic.addEventListener('click', showLock);
-
-   	decryptBtn.addEventListener('click', lockBtnAction);
-
-	verifyBtn.addEventListener('click', signVerify);
-
-   	main2extraBtn.addEventListener('click', main2extra);
-
-   	decryptBtnBasic.addEventListener('click', lockBtnAction);
-
-	decryptBtnEmail.addEventListener('click', lockBtnAction);
-
-   	extra2mainBtn.addEventListener('click', main2extra);
+   	replyBtn.addEventListener('click', lockItem);
 
    	niceEditBtn.addEventListener('click', toggleRichText);
 
-   	chatBtn.addEventListener('click', Chat);
+	inviteBtn.addEventListener('click', makeInvite);
 
-   	sendSMSBtn.addEventListener('click', sendSMS);
+	hideBtn.addEventListener('click', hideBtnAction);
 
-   	secretShareBtn.addEventListener('click', splitJoin);
+	resetBtn.addEventListener('click', resetPFS);
 
-	stegoBtn.addEventListener('click', textStego);
+	moveBtn.addEventListener('click', moveDB);
 
-	stegoBtnEmail.addEventListener('click', textStego);
+   	makeChatBtn.addEventListener('click', startChat);
 
-   	image2mainBtn.addEventListener('click', image2main);
+	submitChatBtn.addEventListener('click', makeChat);
 
-   	lock2dirBtn.addEventListener('click', lock2dir);
+   	cancelChatBtn.addEventListener('click', cancelChat);
 
-   	clearLocksBtn.addEventListener('click', clearLocks);
+	suggestKeyBtn.addEventListener('click', suggestKey);
 
-   	addLockBtn.addEventListener('click', function(){addLock(false)});
+	cancelCoverBtn.addEventListener('click', cancelCover);
 
-   	removeLockBtn.addEventListener('click', removeLock);
+	acceptCoverBtn.addEventListener('click', textStego);
 
-   	resetPFSBtn.addEventListener('click', resetPFS);
+	nameList.addEventListener('change', loadLock);
 
-   	showLockDBBtn.addEventListener('click', showLockDB);
+	nameList2.addEventListener('change', loadName);
 
-   	mergeLockDBBtn.addEventListener('click', mergeLockDB);
+	cancelSelectBtn.addEventListener('click', cancelSelect);
 
-   	moveLockDBBtn.addEventListener('click', moveLockDB);
+	acceptSelectBtn.addEventListener('click', acceptSelect);
 
-   	acceptKeyBtn.addEventListener('click', acceptpwd);
+	acceptNameBtn.addEventListener('click', storeNewLock);
 
-   	cancelKeyBtn.addEventListener('click', cancelKey);
+	cancelResetBtn.addEventListener('click', cancelReset);
 
-   	skipIntroBtn.addEventListener('click', cancelKey);
+	acceptResetBtn.addEventListener('click', acceptReset);
 
-   	changeNameBtn.addEventListener('click', showName);
+	pwd.addEventListener('keyup', function(event) {pwdKeyup(event)}, false);
 
-   	changeKeyBtn.addEventListener('click', acceptnewKey);
+	oldPwd.addEventListener('keyup', function(event) {oldPwdKeyup(event)}, false);
 
-   	changeEmailBtn.addEventListener('click', showEmail);
-
-   	backupSettingsBtn.addEventListener('click', moveMyself);
-
-   	basicMode.addEventListener('click', mode2basic);
-
-   	advancedMode.addEventListener('click', mode2adv);
-
-	emailMode.addEventListener('click', mode2email);
-
-	liteStyle.addEventListener('click', selectStyle);
-
-	darkStyle.addEventListener('click', selectStyle);
-
-	redStyle.addEventListener('click', selectStyle);
-
-	greenStyle.addEventListener('click', selectStyle);
-
-	blueStyle.addEventListener('click', selectStyle);
-
-	customStyle.addEventListener('click', selectStyle);
-
-	colorPicker.addEventListener('change',updateColor);
-
-	rndColors.addEventListener('click', randomColors);
-
-	editTabColor.addEventListener('click', initPicker);
-
-	editBgColor.addEventListener('click', initPicker);
-
-	editBtnColor.addEventListener('click', initPicker);
-
-	editBoxColor.addEventListener('click', initPicker);
-
-	anonMode.addEventListener('click', checkboxStore);
-
-	signedMode.addEventListener('click', checkboxStore);
-
-	onceMode.addEventListener('click', checkboxStore);
-
-	learnMode.addEventListener('click', checkboxStore);
-
-	longMode.addEventListener('click', checkboxStore);
-
-	shortMode.addEventListener('click', checkboxStore);
-
-	compatMode.addEventListener('click', checkboxStore);
-
-	decoyMode.addEventListener('click', checkboxStore);
-
-	wordLockMode.addEventListener('click', checkboxStore);
-
-	ezLokMode.addEventListener('click', checkboxStore);
-
-   	normalLockMode.addEventListener('click', checkboxStore);
-
-	fileMode.addEventListener('click', checkboxStore);
-
-	binaryMode.addEventListener('click', checkboxStore);
-
-	textMode.addEventListener('click', checkboxStore);
-
-	chromeSyncMode.addEventListener('click', checkboxStore);
-
-	sentenceMode.addEventListener('click', checkboxStore);
-
-	letterMode.addEventListener('click', checkboxStore);
-
-	invisibleMode.addEventListener('click', checkboxStore);
-
-	wordMode.addEventListener('click', checkboxStore);
-
-	spaceMode.addEventListener('click', checkboxStore);
-
-	pwdIcon.addEventListener('click',function(){showPwd('pwd')});
-
-   	introRandomBtn.addEventListener('click', randomToken);
-
-	clearIntroRandomBtn.addEventListener('click', clearIntroEmail);
-
-   	randomEmailBtn.addEventListener('click', randomToken);
-
-   	acceptEmailBtn.addEventListener('click', email2any);
-
-   	cancelEmailBtn.addEventListener('click', cancelEmail);
-
-   	acceptNameBtn.addEventListener('click', name2any);
-
-   	cancelNameBtn.addEventListener('click', cancelName);
-
-   	pwdIntroIcon.addEventListener('click', function(){showPwd('pwdIntro')});
-
-   	clearIntroBtn.addEventListener('click', clearIntro);
-
-   	suggestIntroBtn.addEventListener('click', suggestIntro);
-
-   	showlockIntroBtn.addEventListener('click', initUser);
-
-   	decoyInIcon.addEventListener('click', function(){showPwd('decoyIn')});
-
-   	submitDecoyBtn.addEventListener('click', acceptdecoyIn);
-
-   	cancelDecoyBtn.addEventListener('click', cancelDecoy);
-
-   	decoyOutIcon.addEventListener('click', function(){showPwd('decoyOut')});
-
-   	submitDecoy2Btn.addEventListener('click', acceptdecoyIn);
-
-   	cancelDecoy2Btn.addEventListener('click', cancelDecoy);
-
-   	submitPartsBtn.addEventListener('click', submitParts);
-
-   	cancelPartsBtn.addEventListener('click', cancelPartsIn);
-
-	closeLockdirBtn.addEventListener('click', lock2dir);
-
-   	cancelChatBtn.addEventListener('click', closeBox);
-
-   	submitChatBtn.addEventListener('click', makeChat);
-
-	lockList.addEventListener('change', fillBox);
-
-	lockList.addEventListener('click', updateButtons);
-
-   	resetListBtn.addEventListener('click', resetList);
-
-	main2lockBtn.addEventListener('click', main2lock);
-
-	lock2mainBtn.addEventListener('click', main2lock);
-
-   	newUserBtn.addEventListener('click', newUser);
-
-   	submitKeyChangeBtn.addEventListener('click', acceptnewKey);
-
-   	cancelKeyChangeBtn.addEventListener('click', cancelKeyChange);
-
-	newKeyIcon.addEventListener('click',function(){showPwd('newKey')});
-
-	gotointro2.addEventListener('click', go2intro2);
-
-   	backtointro1.addEventListener('click', go2intro2);
-
-   	gotointro3.addEventListener('click', go2intro3);
-
-   	backtointro2.addEventListener('click', go2intro3);
-
-   	gotointro4.addEventListener('click', go2intro4);
-
-   	backtointro3.addEventListener('click', go2intro4);
-
-   	gotointro5.addEventListener('click', go2intro5);
-
-   	backtointro4.addEventListener('click', go2intro5);
-
-   	mainBox.addEventListener('keyup', charsLeft);
-
-	mainBox.addEventListener('paste', pasteMain);
-
-   	decoyText.addEventListener('keyup', charsLeft);
-
-   	chatDate.addEventListener('keyup', charsLeft);
-
-	pwdBox.addEventListener('keyup',function(event){boxKeyup('pwd',event)});
-
-	pwdIntroBox.addEventListener('keyup',function(event){boxKeyup('pwdIntro',event)});
-
-	decoyInBox.addEventListener('keyup', function(event){boxKeyup('decoyIn',event)});
-
-	decoyOutBox.addEventListener('keyup', function(event){boxKeyup('decoyOut',event)});
-
-	partsIn.addEventListener('keyup', function(event) {partsKeyup(event)}, false);
-
-	newKeyBox.addEventListener('keyup', function(event){boxKeyup('newKey',event)});
-
-	newKey2Box.addEventListener('keyup', function(event) {newKey2up(event)}, false);
-
-	lockBox.addEventListener('keyup', function(event) {lockBoxKeyup(event)}, false);
-
-	lockBox.addEventListener('paste', pasteLock);
-
-	userNameBox.addEventListener('keyup', function(event) {nameKeyup(event)}, false);
-
-	emailBox.addEventListener('keyup', function(event) {emailKeyup(event)}, false);
-
-	imageBox.addEventListener('keyup', function(event){boxKeyup('image',event)});
+	nameBox.addEventListener('keyup', function(event) {nameKeyup(event)}, false);
 
 //for the rich text editor boxes and buttons
 	formatBlock.addEventListener("change", function() {formatDoc('formatBlock',this[this.selectedIndex].value);this.selectedIndex=0;});
@@ -329,27 +101,27 @@ window.onload = function() {
 	foreColor.addEventListener("change", function() {formatDoc('foreColor',this[this.selectedIndex].value);this.selectedIndex=0;});
 	backColor.addEventListener("change", function() {formatDoc('backColor',this[this.selectedIndex].value);this.selectedIndex=0;});
 
-	document.images[0].addEventListener("click", function() {formatDoc('bold')});
-	document.images[1].addEventListener("click", function() {formatDoc('italic')});
-	document.images[2].addEventListener("click", function() {formatDoc('underline')});
-	document.images[3].addEventListener("click", function() {formatDoc('strikethrough')});
-	document.images[4].addEventListener("click", function() {formatDoc('subscript')});
-	document.images[5].addEventListener("click", function() {formatDoc('superscript')});
-	document.images[6].addEventListener("click", function() {formatDoc('justifyleft')});
-	document.images[7].addEventListener("click", function() {formatDoc('justifycenter')});
-	document.images[8].addEventListener("click", function() {formatDoc('justifyright')});
-	document.images[9].addEventListener("click", function() {formatDoc('justifyfull')});
-	document.images[10].addEventListener("click", function() {formatDoc('insertorderedlist')});
-	document.images[11].addEventListener("click", function() {formatDoc('insertunorderedlist')});
-	document.images[12].addEventListener("click", function() {formatDoc('formatBlock','blockquote')});
-	document.images[13].addEventListener("click", function() {formatDoc('outdent')});
-	document.images[14].addEventListener("click", function() {formatDoc('indent')});
-	document.images[15].addEventListener("click", function() {formatDoc('inserthorizontalrule')});
-	document.images[16].addEventListener("click", function() {var sLnk=prompt('Write the URL here','http:\/\/');if(sLnk&&sLnk!=''&&sLnk!='http://'){formatDoc('createlink',sLnk)}});
-	document.images[17].addEventListener("click", function() {formatDoc('unlink')});
-	document.images[18].addEventListener("click", function() {formatDoc('removeFormat')});
-	document.images[19].addEventListener("click", function() {formatDoc('undo')});
-	document.images[20].addEventListener("click", function() {formatDoc('redo')});
+	document.images[1].addEventListener("click", function() {formatDoc('bold')});
+	document.images[2].addEventListener("click", function() {formatDoc('italic')});
+	document.images[3].addEventListener("click", function() {formatDoc('underline')});
+	document.images[4].addEventListener("click", function() {formatDoc('strikethrough')});
+	document.images[5].addEventListener("click", function() {formatDoc('subscript')});
+	document.images[6].addEventListener("click", function() {formatDoc('superscript')});
+	document.images[7].addEventListener("click", function() {formatDoc('justifyleft')});
+	document.images[8].addEventListener("click", function() {formatDoc('justifycenter')});
+	document.images[9].addEventListener("click", function() {formatDoc('justifyright')});
+	document.images[10].addEventListener("click", function() {formatDoc('justifyfull')});
+	document.images[11].addEventListener("click", function() {formatDoc('insertorderedlist')});
+	document.images[12].addEventListener("click", function() {formatDoc('insertunorderedlist')});
+	document.images[13].addEventListener("click", function() {formatDoc('formatBlock','blockquote')});
+	document.images[14].addEventListener("click", function() {formatDoc('outdent')});
+	document.images[15].addEventListener("click", function() {formatDoc('indent')});
+	document.images[16].addEventListener("click", function() {formatDoc('inserthorizontalrule')});
+	document.images[17].addEventListener("click", function() {var sLnk=prompt('Write the URL here','http:\/\/');if(sLnk&&sLnk!=''&&sLnk!='http://'){formatDoc('createlink',sLnk)}});
+	document.images[18].addEventListener("click", function() {formatDoc('unlink')});
+	document.images[19].addEventListener("click", function() {formatDoc('removeFormat')});
+	document.images[20].addEventListener("click", function() {formatDoc('undo')});
+	document.images[21].addEventListener("click", function() {formatDoc('redo')});
 
 //for the help screens
 	var helpHeaders = document.getElementsByClassName("helpHeading");		//add listeners to all the help headers
@@ -363,34 +135,23 @@ window.onload = function() {
 	for (var i = 0; i < helpHeaders2.length; i++) {
 		helpHeaders2[i].addEventListener('click', openHelp2);
 	}
-
+	
 //fixes after inline styles were moved to css file
-	lockList.style.padding = '4px';
-	lockList.style.width = '30%';
-	basicBtnsTop.style.display = 'block';
-	mainMsg.style.minHeight = '20px';
-	extraButtonsTop.style.display = 'none'
+	mainScr.style.display = 'block';
+	fileOptions.style.display = 'none'
 };
 
-//for general directory
-window.addEventListener('message', receiveMessage, false);
-
-//gets Lock from the general directory iframe and puts it in Lock screen
-function receiveMessage(evt){
-  	if (evt.origin === 'https://www.passlok.com'){
-    	lockBox.textContent = evt.data;
-		suspendFindLock = true;
-		lockMsg.textContent='Give a name to this Lock and save it. Otherwise Clear.'
-  	}
-}
-
-if(!localStorage){newUser();}else if(localStorage.length == 0){newUser();};
-
-fillNameList();
-
-initTabs();																	//initialize tabs
-
-//var time10 = hashTime10();													//get milliseconds for 10 wiseHash at iter = 10
+//var time10 = hashTime10();											//get milliseconds for 10 wiseHash at iter = 10
 var time10 = 200									//valid for core2 duo. Should be smaller for more recent machines
+
+//mainBox.innerText = decodeURI(window.location.hash).slice(1);			//correspondent's message from address bar
+var theirezLock = '', theirLock = '', theirName = '';
+if(window.location.hash){
+	mainBox.textContent = (decodeURI(window.location.hash).slice(1).match('==(.*)==') || [' ',' '])[1]
+
+	theirezLock = mainBox.textContent.slice(0,50),
+	theirLock = changeBase(theirezLock, base36, base64, true),
+	theirName = ''
+}
 
 //end of body script.
