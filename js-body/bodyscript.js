18889d490812a1c8b7007870222f4c4739d0e70f
Francisco Ruiz <fruiz500@gmail.com>
2020-05-14 16:42:47 -0500

    v2.4.18 real
    the real bodyscript.js file

--- js-body/bodyscript.js
+++ js-body/bodyscript.js
@@ -19,78 +19,53 @@
 //  Clear out "sorry, no JavaScript" warning and display the type of source
 	showGreeting();
 
-//set global variable indicating if there is a Chrome sync data area. Works for Chrome apps and extension
+//set global variable indicating if there is a Chrome sync data area. Works for Chrome and Firefox apps and extension
 var ChromeSyncOn = false;
-if(isChrome){
+if(chrome){
 	if(chrome.storage){
 		if(chrome.storage.sync){
-			ChromeSyncOn = true;
-			retrieveAllSync();
+			ChromeSyncOn = true
 		}
 	}
 }
 
 //clears the no JavaScript warning and displays an initial message depending on the type of source
 function showGreeting(){
-	var protocol = window.location.protocol,
-		msgStart = "Welcome to SeeOnce\r\n",
-		msgEnd = "\r\nEnter your Password and click OK";
+	var protocol = window.location.protocol;
+	var msgStart = "Welcome to PassLok Privacy\r\n",
+		msgEnd = "\r\nSelect your user name and enter your Key. Then click OK";
 	if(protocol == 'file:'){
-		keyMsg.textContent = msgStart + 'running from a local file' + msgEnd
+		pwdMsg.textContent = msgStart + 'running from a saved file' + msgEnd
 	}else if(protocol == 'https:'){
-		keyMsg.textContent = msgStart + 'downloaded from a secure server' + msgEnd
-	}else if(protocol == 'chrome-extension:'){
-		keyMsg.textContent = msgStart + 'running as a Chrome app' + msgEnd
+		pwdMsg.textContent = msgStart + 'downloaded from a secure server' + msgEnd
+	}else if(protocol == 'chrome-extension:' || protocol == 'moz-extension:'){
+		pwdMsg.textContent = msgStart + 'running as a Chrome or Firefox addon' + msgEnd
 	}else{
-		mainScr.style.backgroundColor = '#ffd0ff';
-		keyMsg.textContent = msgStart + 'WARNING: running from an insecure source!' + msgEnd
+		mainTab.style.backgroundColor = '#ffd0ff';
+		pwdMsg.textContent = msgStart + 'WARNING: running from an insecure source!' + msgEnd
 	}
-	if(!isMobile) pwd.focus()
-
-	//display special greeting the first time the program runs. The Chrome app does this differently
-	if(!ChromeSyncOn){if(!localStorage){introGreeting();}else if(!localStorage['locDir']){introGreeting();}}
-}
-
-//special instructions the first time it runs
-function introGreeting(){
-	firstTimeKey.style.display = 'block';
-	keyMsg.textContent = 'The strength will appear here. Enter the Password and click OK';
-	if(isiPhone || isAndroidPhone){
-		keyScr.style.width = '100%';			//more space needed for phones
-		keyScr.style.height = '100%';
-		keyScr.style.top = 0;
-		keyScr.style.left = 0;
-		firstTimeKey.style.fontSize = 'large'	//smaller font
-	}else if(isMobile){
-		firstTimeKey.style.fontSize = 'large'
-	}
-}
-function cancelIntroGreeting(){
-	keyScr.style.width = '';
-	keyScr.style.height = '';
-	keyScr.style.top = '';
-	keyScr.style.left = '';
-	firstTimeKey.style.display = 'none'
 }
 
 //resizes text boxes so they fit within the window
 function textheight(){
 	var	fullheight = document.documentElement.clientHeight,
-		offsetheight = 400,
+		offsetheight = 392,
 		toolbarheight = 50;
-	if(isiPhone) offsetheight = offsetheight - 70;
-	coverBox.style.height = fullheight - offsetheight + 50 + 'px';
+	if(isiPhone) offsetheight = offsetheight - 65;
+	lockBox.style.height = (fullheight - 300) * (isMobile ? 1 : 0.8) + 'px';
 	if(niceEditor){
 		mainBox.style.height = fullheight - offsetheight - toolbarheight + 'px'
 	}else{
 		if(isMobile){
 			if(isAndroid && !isFile){
-				mainBox.style.height = fullheight - offsetheight + 80 + 'px';
-			}else{
 				mainBox.style.height = fullheight - offsetheight + 40 + 'px';
+			}else if(isiPhone){
+				mainBox.style.height = fullheight - offsetheight - 10 + 'px';
+			}else{
+				mainBox.style.height = fullheight - offsetheight + 'px';
 			}
 		}else{
-			mainBox.style.height = fullheight - offsetheight + 'px';
+			mainBox.style.height = fullheight - offsetheight - 10 + 'px';
 		}
 	}
 }
@@ -130,8 +105,7 @@ function loadFileAsURL(){
 			fileLink.textContent = fileName;
 			mainBox.appendChild(fileLink)
 		}
-		if(fileName.match(/.(plk|txt)/)) Decrypt()				//in case it is an encrypted message, attempt to decrypt it
-	}
+	};
 	if(fileToLoad.type.slice(0,4) == "text"){
 		fileReader.readAsText(fileToLoad, "UTF-8");
 		mainMsg.textContent = 'This is the content of file: ' + fileToLoad.name;
@@ -139,6 +113,32 @@ function loadFileAsURL(){
 		fileReader.readAsDataURL(fileToLoad, "UTF-8");
 		mainMsg.textContent = 'The file has been loaded in encoded form. It is NOT ENCRYPTED';
 	}
+	setTimeout(function(){
+		updateButtons();
+		if(decryptBtn.textContent == 'Decrypt') mainMsg.textContent = 'This file may contain an encrypted item';
+		if(verifyBtn.textContent == 'Unseal') mainMsg.textContent = 'This file may contain a sealed item';
+		if(secretShareBtn.textContent == 'Join') mainMsg.textContent = 'This file may contain a part. Add more parts if necessary';
+	},300);
+}
+
+//to load a file into the directory dialog
+function loadLockFile(){
+	var fileToLoad = lockFile.files[0],
+		fileReader = new FileReader();
+	fileReader.onload = function(fileLoadedEvent){
+		var fileName = fileToLoad.name;
+			URLFromFileLoaded = fileLoadedEvent.target.result,
+			escapedName = escapeHTML(fileName);
+		lockBox.textContent = '';
+		var fileLink = document.createElement('a');
+		fileLink.download = escapedName;
+		fileLink.href = decryptSanitizer(URLFromFileLoaded).replace(/=+$/,'');
+		fileLink.textContent = escapedName;
+		lockBox.appendChild(fileLink)
+	};
+
+	fileReader.readAsDataURL(fileToLoad, "UTF-8");
+	lockMsg.textContent = 'File ' + escapedName + ' has been loaded'
 }
 
 //to load an image into the main box
